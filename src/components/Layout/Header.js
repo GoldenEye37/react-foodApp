997d7dd3f2d408d50d3e2b35d278e24d2ae362e9
Leon Dhimba <LDhimba@bancabc.co.zw>
2022-09-22 11:10:44 +0200

    foodapp initial

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Fragment } from 'react';
+import meals from '../../assets/meals.jpg';
+
+import classes from './Header.module.css';
+import HeaderCardButton from './HeaderCardButton';
+
+const Header = (props) => {
+
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCardButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt='A table full of delicious food!'/>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
---
+++ src/components/Layout/HeaderCardButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCardButton.module.css";
+
+const HeaderCardButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span className={classes.badge}>Your Cart</span>
+      <span>3</span>
+    </button>
+  );
+};
+
+export default HeaderCardButton;

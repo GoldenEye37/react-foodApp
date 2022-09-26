06075b77cfbde3c59f97e80c74c4397d4a9ced55
Leon Dhimba <LDhimba@bancabc.co.zw>
2022-09-26 09:37:47 +0200

    create cart context and provider

---
+++ src/store/CartProvider.js
@@ -0,0 +1,26 @@
+ import CartContext from './cart-context';
+import React from 'react';
+
+
+
+const CartProvider = (props) => {
+
+    const addItemToCartHandler = (item) => {};
+
+    const removeItemFromCartHandler = (id) => {};
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler ,
+        removeItem: removeItemFromCartHandler,
+    };
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
